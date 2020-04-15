ecb94122476faaf1be762f67338c2d1c713bfe51
Evan W. Patton <ewpatton@mit.edu>
2020-04-14 23:51:03 -0400

    Bump version to 0.1.3
    Change-Id: I7e0137ffde2fc929119d5df43b1cff182558c40c

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='appinventor-tfjs',
-      version='0.1.2',
+      version='0.1.3',
       description='Tool for generating App Inventor extension skeletons from Tensorflow.js models',
       long_description=long_description,
       long_description_content_type='text/markdown',
